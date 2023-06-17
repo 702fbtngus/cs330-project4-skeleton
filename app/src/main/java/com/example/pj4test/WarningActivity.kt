6228d98fccf5986eb1ffd3aff9979f8d528e033f
Haeseul <jjchsjj@gmail.com>
2023-06-18 03:23:34 +0900

    Add alarm sound at warning pages

--- app/src/main/java/com/example/pj4test/WarningActivity.kt
+++ app/src/main/java/com/example/pj4test/WarningActivity.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.media.MediaPlayer
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
@@ -13,6 +14,7 @@ import java.util.*
 class WarningActivity : AppCompatActivity() {
     private val TAG = "WarningActivity"
     var background: TextView? = null
+    var mediaPlayer: MediaPlayer? = null
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,9 +28,13 @@ class WarningActivity : AppCompatActivity() {
     }
 
     fun addListenerOnView() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.clock_tick);
+        mediaPlayer?.isLooping = true;
+        mediaPlayer?.start();
         val context = this
         background = findViewById<View>(R.id.warning_background) as TextView
         background!!.setOnClickListener {
+            mediaPlayer?.stop()
             finish()
         }
     }
